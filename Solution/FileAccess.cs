b85d48de7966c9fc6ecb9678d8426c57ac5c0d5a
Nitin <ch.nitin11@gmail.com>
2016-11-10 14:59:30 +0530

    Initial solution

---
+++ Solution/FileAccess.cs
@@ -0,0 +1,103 @@
+﻿namespace CourseraAlgoSolution
+{
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+
+    /// <summary>
+    /// Access the input and log files
+    /// </summary>
+    class FileAccess
+    {
+        /// <summary>
+        /// Input file path
+        /// </summary>
+        private string inputDirectory = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName) + @"\dataFiles";
+        /// <summary>
+        /// Log file path
+        /// </summary>
+        private string logDirectory = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName) + @"\logs";
+        /// <summary>
+        /// Access files
+        /// </summary>
+        internal FileAccess()
+        {
+            Directory.CreateDirectory(logDirectory);
+            StartLog();
+        }
+        /// <summary>
+        /// Access files
+        /// </summary>
+        /// <param name="inputDirectory">Path for input file</param>
+        internal FileAccess(string inputDirectory)
+        {
+            Directory.CreateDirectory(logDirectory);
+            this.inputDirectory = inputDirectory;
+            StartLog();
+        }
+
+        /// <summary>
+        /// Reads the input file as a single string
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public string ReadAsText(string fileName)
+        {
+            // put extension .txt (bt default), if no extension is present
+            fileName += fileName.Contains(".") ? "" : ".txt";
+            try
+            {
+                return File.ReadAllText(inputDirectory + "/" + fileName);
+            }
+            catch {
+                LogMessage("File read error.");
+                throw;
+            }
+        }
+        /// <summary>
+        /// Reads the lines in input file as different strings
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public string[] ReadAsArray(string fileName)
+        {
+            // put extension .txt (bt default), if no extension is present
+            fileName += fileName.Contains(".") ? "" : ".txt";
+            try
+            {
+                return File.ReadAllLines(inputDirectory + "/" + fileName);
+            }
+            catch
+            {
+                LogMessage("File read error.");
+                throw;
+            }
+        }
+        private void StartLog() {
+            using (StreamWriter logFile = File.AppendText(logDirectory + "/" + DateTime.Now.ToString("_yyyy_MM_dd_HH")))
+            {   
+                logFile.WriteLine(Environment.NewLine);
+                logFile.WriteLine("Program run started at: " + DateTime.Now.ToString("hh:mm:ss.ffffff"));
+                logFile.Write(Environment.NewLine);
+            }
+        }
+        /// <summary>
+        /// Logs text in log file
+        /// </summary>
+        /// <param name="message">Message to be logged</param>
+        /// <returns></returns>
+        public bool LogMessage(string message) {
+            try
+            {
+                using (StreamWriter logFile = File.AppendText(logDirectory + "/" + DateTime.Now.ToString("_yyyy_MM_dd_HH")))
+                {
+                    logFile.WriteLine(DateTime.Now.ToString("hh:mm:ss.ffffff") + ": " + message);
+                }
+                return true;
+            }
+            catch (Exception) {
+                return false;
+            }
+        }
+    }
+}
---
+++ Solution/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseraAlgoSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fileContent;
+            FileAccess file = new FileAccess();
+            file.LogMessage("File read started..");
+            try
+            {
+                fileContent = file.ReadAsArray("quicksort_w2_pq1");
+                file.LogMessage("File read ended..");
+                foreach (string data in fileContent) {
+                    int i;
+                    Int32.TryParse(data, out i);
+                }
+                file.LogMessage("Process ended..");
+            }
+            catch (Exception ex) {
+                file.LogMessage(ex.Message);
+            }
+        }
+    }
+}
