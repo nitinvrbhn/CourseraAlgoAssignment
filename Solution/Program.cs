50b6503ea2e91661a999b383dc019c212f5b30aa
nitin <ch.nitin11@gmail.com>
2017-02-26 15:32:46 +0530

    Commitin the code

--- Solution/Program.cs
+++ Solution/Program.cs
@@ -21,11 +21,68 @@ namespace CourseraAlgoSolution
                     int i;
                     Int32.TryParse(data, out i);
                 }
-                file.LogMessage("Process ended..");
+                List<int[]> dataArray = new List<int[]>();
+                foreach (var i in fileContent)
+                {
+                    dataArray.Add(Array.ConvertAll(i.Split(' '), int.Parse));
+                }
+                int[] distance = Dijkstra(dataArray.ToArray(), dataArray.Count);
+                file.LogMessage("Process ended.." + distance.ToString());
             }
             catch (Exception ex) {
                 file.LogMessage(ex.Message);
             }
         }
+        private static int MinimumDistance(int[] distance, bool[] shortestPathTreeSet, int verticesCount)
+        {
+            int min = int.MaxValue;
+            int minIndex = 0;
+
+            for (int v = 0; v < verticesCount; ++v)
+            {
+                if (shortestPathTreeSet[v] == false && distance[v] <= min)
+                {
+                    min = distance[v];
+                    minIndex = v;
+                }
+            }
+
+            return minIndex;
+        }
+
+        private static void Print(int[] distance, int verticesCount)
+        {
+            Console.WriteLine("Vertex    Distance from source");
+
+            for (int i = 0; i < verticesCount; ++i)
+                Console.WriteLine("{0}\t  {1}", i, distance[i]);
+        }
+
+        public static int[] Dijkstra(int[][] graph, int verticesCount)
+        {
+            int[] distance = new int[verticesCount];
+            bool[] shortestPathTreeSet = new bool[verticesCount];
+
+            for (int i = 0; i < verticesCount; ++i)
+            {
+                distance[i] = int.MaxValue;
+                shortestPathTreeSet[i] = false;
+            }
+            
+
+            for (int count = 0; count < verticesCount - 1; ++count)
+            {
+                int u = MinimumDistance(distance, shortestPathTreeSet, verticesCount);
+                shortestPathTreeSet[u] = true;
+
+                for (int v = 0; v < verticesCount; ++v)
+                    if (!shortestPathTreeSet[v] && Convert.ToBoolean(graph[u][v]) && distance[u] != int.MaxValue && distance[u] + graph[u][v] < distance[v])
+                        distance[v] = distance[u] + graph[u][v];
+            }
+
+            return distance;
+        }
+
     }
+
 }
