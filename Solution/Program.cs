b91b76066a2cf29c6f9759315bbc4a29aa58b2c3
nitin <ch.nitin11@gmail.com>
2017-02-26 16:18:24 +0530

    Commiting the code

--- Solution/Program.cs
+++ Solution/Program.cs
@@ -21,11 +21,57 @@ namespace CourseraAlgoSolution
                     int i;
                     Int32.TryParse(data, out i);
                 }
-                file.LogMessage("Process ended..");
+                int[] dataArray = Array.ConvertAll(fileContent, int.Parse);
+                dataArray = twoSum(dataArray, dataArray[1]);
+                file.LogMessage("Process ended.. "+ dataArray.ToString());
             }
             catch (Exception ex) {
                 file.LogMessage(ex.Message);
             }
+        }
+        public static int[] twoSum(int[] numbers, int target)
+        {
+            var dictionary = new Dictionary<int, List<int>>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (dictionary.ContainsKey(numbers[i]))
+                {
+                    dictionary[numbers[i]].Add(i);
+                }
+                else
+                {
+                    dictionary.Add(numbers[i], new List<int> { i });
+                }
+            }
+
+            foreach (var keyValuePair in dictionary)
+            {
+                List<int> listOfIndexes;
+                var remained = target - keyValuePair.Key;
+                if (!dictionary.TryGetValue(remained, out listOfIndexes))
+                {
+                    continue;
+                }
+
+
+                if (remained == keyValuePair.Key && listOfIndexes.Count > 1)
+                {
+                    var result = listOfIndexes.GetRange(0, 2);
+                    result.Sort();
+                    return result.ToArray();
+                }
+
+                if (remained != keyValuePair.Key && listOfIndexes.Count > 0)
+                {
+                    var result = new List<int>() { keyValuePair.Value[0], listOfIndexes[0] };
+                    result.Sort();
+
+                    return result.ToArray();
+                }
+            }
+
+            return null;
         }
     }
 }
